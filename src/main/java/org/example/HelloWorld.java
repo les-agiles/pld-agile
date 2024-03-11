c31319a9fa3c07e071feccc4bcfebe9bfd3bfec1
Xtz_JoC <xtz_joc@netcourrier.com>
2024-03-11 18:03:51 +0100

    Ajout d'une fenetre swing HelloWorld

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class HelloWorld {
+    private JPanel rootPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("HelloWorld");
+        frame.setContentPane(new HelloWorld().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
