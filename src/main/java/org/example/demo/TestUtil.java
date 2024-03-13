1d93091f39d6dcfd54e440c9257e698c27253ee3
Nassim MANSOUR <mansournassim7@gmail.com>
2024-03-13 11:18:35 +0100

    Adding test step to maven.yml pipeline

---
+++ src/main/java/org/example/demo/TestUtil.java
@@ -0,0 +1,11 @@
+package org.example.demo;
+
+public class TestUtil {
+    public static String getGreeting() {
+        return "Hello, world!";
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
