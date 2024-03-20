8005f809e2e1990c3a57cb8c9d3308e2c76a8e8c
Guilhem RICHAUD <50924532+guiphaniel@users.noreply.github.com>
2024-03-20 14:02:33 +0100

    fix de la map (version maven)

--- src/main/java/fr/insa/geofast/HelloApplication.java
+++ src/main/java/fr/insa/geofast/HelloApplication.java
@@ -9,19 +9,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
+    protected static final Logger logger = LogManager.getLogger();
+
     @Override
     public void start(Stage stage) throws IOException {
         Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
 
         stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResourceAsStream("GeoFastIcon.png"))));
 
+        String fxmlFile = "map-view.fxml";
+        logger.debug("loading fxml file {}", fxmlFile);
         FXMLLoader fxmlLoader = new FXMLLoader();
-        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream("map-view.fxml"));
+        Parent rootNode = fxmlLoader.load(getClass().getResourceAsStream(fxmlFile));
+        logger.trace("stage loaded");
 
         final MapController controller = fxmlLoader.getController();
         final Projection projection = getParameters().getUnnamed().contains("wgs84")
@@ -29,9 +36,14 @@ public class HelloApplication extends Application {
         controller.initMapAndControls(projection);
 
         Scene scene = new Scene(rootNode);
+        logger.trace("scene created");
+
         stage.setTitle("GeoFast!");
         stage.setScene(scene);
+        logger.trace("showing scene");
         stage.show();
+
+        logger.debug("application start method finished.");
     }
 
     public static void main(String[] args) {
