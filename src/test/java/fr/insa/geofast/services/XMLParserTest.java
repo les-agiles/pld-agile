5f74506aaa35112beccea31192a746be9b15df54
Eren Mustafa C <pro.erenmustafa.ceylan@gmail.com>
2024-03-20 12:16:10 +0100

    XMLParserTest : Test de la MAP basic avec le Warehouse Basic, Fichier de test basic qui convient

---
+++ src/test/java/fr/insa/geofast/services/XMLParserTest.java
@@ -0,0 +1,35 @@
+package fr.insa.geofast.services;
+
+import fr.insa.geofast.models.Intersection;
+import fr.insa.geofast.models.Map;
+import fr.insa.geofast.models.Warehouse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class XMLParserTest {
+    private static String absolutePath;
+
+    @BeforeAll
+    public static void setUp() {
+        Path resourceDirectory = Paths.get("src", "test", "java", "resources");
+        absolutePath = resourceDirectory.toFile().getAbsolutePath();
+    }
+
+    @Test
+    public void testParseMap() {
+        Map map = null;
+        try {
+            map = XMLParser.parseMap(absolutePath + "/unit-tests-map1.xml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(map);
+        Assertions.assertNotNull(map);
+        Assertions.assertEquals(1, map.getWarehouse().getAddress());
+
+    }
+}
