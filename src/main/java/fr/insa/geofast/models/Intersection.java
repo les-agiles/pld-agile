04c2b259fd922eb8b7df46c575aabfecbd070388
Eren Mustafa C <pro.erenmustafa.ceylan@gmail.com>
2024-03-20 10:10:24 +0100

    Ajout de la liste des segments à l'intersection

--- src/main/java/fr/insa/geofast/models/Intersection.java
+++ src/main/java/fr/insa/geofast/models/Intersection.java
@@ -1,9 +1,12 @@
 package fr.insa.geofast.models;
+
 import lombok.Getter;
+import java.util.List;
 
 @Getter
 public class Intersection {
     private String id;
     private double latitude;
     private double longitude;
+    private List<Segment> segments;
 }
