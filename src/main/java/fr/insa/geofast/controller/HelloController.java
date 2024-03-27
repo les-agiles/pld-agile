b4e5682fe1d0a7809f9fbc86bc6716ed53fb7772
Nassim MANSOUR <mansournassim7@gmail.com>
2024-03-27 16:41:17 +0100

    Fixing FXML views

--- src/main/java/fr/insa/geofast/controller/HelloController.java
+++ src/main/java/fr/insa/geofast/controller/HelloController.java
@@ -12,7 +12,7 @@ import java.io.FileReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class HelloController {
 
     @FXML
     private LeftController leftController;
@@ -26,53 +26,5 @@ public class HelloController implements Initializable {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
-    private Button importerPlan;
-
-    @FXML
-    private Button importerProgramme;
-
-    @FXML
-    public Button sauvegarderProgramme;
-
-    @FXML
-    public Button exporterProgramme;
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        importerPlan.setOnAction(e -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
-            File selectedFile = fileChooser.showOpenDialog(null);
-            if (selectedFile != null) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
-        importerProgramme.setOnAction(e -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
-            File selectedFile = fileChooser.showOpenDialog(null);
-            if (selectedFile != null) {
-                try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-
     }
 }
