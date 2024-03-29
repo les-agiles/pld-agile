f0be446509930f9790f80c8e14be5a2f1e883f18
Eren Mustafa C <pro.erenmustafa.ceylan@gmail.com>
2024-03-29 16:23:58 +0100

    Cupertino Light Design

--- src/main/java/fr/insa/geofast/GeofastApp.java
+++ src/main/java/fr/insa/geofast/GeofastApp.java
@@ -1,6 +1,6 @@
 package fr.insa.geofast;
 
-import atlantafx.base.theme.PrimerLight;
+import atlantafx.base.theme.CupertinoLight;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,7 +18,7 @@ public class GeofastApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+        Application.setUserAgentStylesheet(new CupertinoLight().getUserAgentStylesheet());
 
         stage.getIcons().add(new Image(Objects.requireNonNull(GeofastApp.class.getResourceAsStream("GeoFastIcon.png"))));
 
