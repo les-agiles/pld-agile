a438d89ba493b5c56389c5ef9c741cad9b01685e
Eren Ceylan <82711747+ErenMustafaGit@users.noreply.github.com>
2024-03-29 15:31:46 +0100

    Error Notification Toaster (#32)
    * Creation of IHMException, and throwing them (MapFactory & PlanningRequestFactory)
    
    * Error Notification toaster for IHMException, clean code on GeofastController
    
    * Test on throw IHMException : MapFactoryTest & PlanningRequestFactoryTest

--- src/main/java/fr/insa/geofast/controller/GeofastController.java
+++ src/main/java/fr/insa/geofast/controller/GeofastController.java
@@ -1,9 +1,24 @@
 package fr.insa.geofast.controller;
 
+import atlantafx.base.controls.Notification;
+import atlantafx.base.theme.Styles;
+import atlantafx.base.util.Animations;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
-public class GeofastController {
+
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class GeofastController extends StackPane implements Initializable {
+
     @FXML
     private LeftController leftController;
 
@@ -11,10 +26,43 @@ public class GeofastController {
     private RightController rightController;
 
     @FXML
-    private Label welcomeText;
+    public StackPane stackPane;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Notification errorNotification;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initNotification();
+
+    }
+
+    private void initNotification() {
+        errorNotification = new Notification(
+                "Une erreur est survenue" // default message
+        );
+        errorNotification.getStyleClass().addAll(
+                Styles.DANGER, Styles.ELEVATED_1
+        );
+        errorNotification.setPrefHeight(Region.USE_PREF_SIZE);
+        errorNotification.setMaxHeight(Region.USE_PREF_SIZE);
+        StackPane.setAlignment(errorNotification, Pos.TOP_RIGHT);
+        StackPane.setMargin(errorNotification, new Insets(10, 10, 0, 0));
+
+        errorNotification.setOnClose(e -> {
+            var out = Animations.slideOutUp(errorNotification, Duration.millis(250));
+            out.setOnFinished(f -> stackPane.getChildren().remove(errorNotification));
+            out.playFromStart();
+        });
+
+        displayNotification("Hello World!");
+    }
+
+    public void displayNotification(String message) {
+        errorNotification.setMessage(message);
+        var in = Animations.slideInDown(errorNotification, Duration.millis(250));
+        if (!stackPane.getChildren().contains(errorNotification)) {
+            stackPane.getChildren().add(errorNotification);
+        }
+        in.playFromStart();
     }
 }
--- src/main/java/fr/insa/geofast/services/PlanningRequestFactory.java
+++ src/main/java/fr/insa/geofast/services/PlanningRequestFactory.java
@@ -1,17 +1,33 @@
 package fr.insa.geofast.services;
 
+import fr.insa.geofast.exceptions.IHMException;
 import fr.insa.geofast.models.Map;
 import fr.insa.geofast.models.PlanningRequest;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
 
+@Slf4j
 public class PlanningRequestFactory {
-    private PlanningRequestFactory(){ }
+    private PlanningRequestFactory() {
+    }
 
-    public static PlanningRequest buildPlanningRequest(String path, Map map) throws JAXBException, FileNotFoundException {
-        PlanningRequest result = XMLParser.parsePlanningRequest(path);
-        result.setup(map);
-        return result;
+    public static PlanningRequest buildPlanningRequest(String path, Map map) throws IHMException {
+        PlanningRequest planningRequest;
+        try {
+            planningRequest = XMLParser.parsePlanningRequest(path);
+            planningRequest.setup(map);
+        } catch (JAXBException e) {
+            log.error(e.getMessage());
+            throw new IHMException("Erreur lors de la lecture du fichier XML");
+        } catch (FileNotFoundException e) {
+            log.error(e.getMessage());
+            throw new IHMException("Fichier introuvable");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new IHMException("Erreur inconnue");
+        }
+        return planningRequest;
     }
 }
