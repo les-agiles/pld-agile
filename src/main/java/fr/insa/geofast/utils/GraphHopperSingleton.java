193d0a75982b409b3fab8072999f5012b80302f0
TheoHauray <theohauray44@gmail.com>
2024-04-02 23:05:53 +0200

    remove tuile

--- src/main/java/fr/insa/geofast/utils/GraphHopperSingleton.java
+++ src/main/java/fr/insa/geofast/utils/GraphHopperSingleton.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -23,7 +22,6 @@ import static com.graphhopper.json.Statement.Op.LIMIT;
 
 @Slf4j
 public class GraphHopperSingleton {
-    private static final String OSM_FILE_LOC = "fr/insa/geofast/tuile/rhone-alpes.osm.pbf";
     private static final String ROUTE_GRAPH_LOC = "fr/insa/geofast/routing-graph/";
 
     @Getter
@@ -35,39 +33,13 @@ public class GraphHopperSingleton {
         CustomModel customModel = new CustomModel().addToSpeed(If("road_class == PRIMARY", LIMIT, "15")).addToSpeed(Else(LIMIT, "15"));
         Profile profile = new Profile("bike").setVehicle("bike").setTurnCosts(false).setCustomModel(customModel);
 
-        Path tempFile = createTempOSMFile();
         Path tempCacheDirectory = createTempCacheDirectory();
 
         graphHopper = new GraphHopper();
-        graphHopper.setOSMFile(tempFile.toString());
         graphHopper.setGraphHopperLocation(tempCacheDirectory.toString());
         graphHopper.setProfiles(profile);
         graphHopper.getCHPreparationHandler().setCHProfiles(new CHProfile("bike"));
         graphHopper.importOrLoad();
-    }
-
-    private Path createTempOSMFile() {
-        InputStream osmStream = Launcher.class.getClassLoader().getResourceAsStream(OSM_FILE_LOC);
-        if (osmStream == null) {
-            throw new IllegalStateException("OSM file not found: " + OSM_FILE_LOC);
-        }
-
-        try {
-            FileAttribute<?> attrs = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwxr-x---"));
-            Path tempFile = Files.createTempFile("rhone-alpes", ".osm.pbf", attrs);
-            tempFile.toFile().deleteOnExit();
-            try (OutputStream out = Files.newOutputStream(tempFile)) {
-                byte[] buffer = new byte[8192];
-                int length;
-                while ((length = osmStream.read(buffer)) > 0) {
-                    out.write(buffer, 0, length);
-                }
-            }
-            return tempFile;
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            throw new IllegalStateException("Failed to create temporary file for OSM data", e);
-        }
     }
 
     private Path createTempCacheDirectory() {
