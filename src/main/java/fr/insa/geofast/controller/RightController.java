09efc9322b90ba4c951644f552e4c08af7078420
XtzJoC <63497580+XtzJoC@users.noreply.github.com>
2024-04-03 10:53:36 +0200

    Ajout du calcul du trajet après appui sur le bouton (#54)
    * Ajout du calcul du trajet après appui sur le bouton
    
    * Fix franglish
    
    * Fix franglish v2

--- src/main/java/fr/insa/geofast/controller/RightController.java
+++ src/main/java/fr/insa/geofast/controller/RightController.java
@@ -1,11 +1,22 @@
 package fr.insa.geofast.controller;
 
+import fr.insa.geofast.exceptions.IHMException;
+import fr.insa.geofast.models.DeliveryGuy;
+import fr.insa.geofast.models.PlanningRequest;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 @Getter
-public class RightController {
+@Slf4j
+public class RightController implements Initializable {
     @Setter
     private GeofastController parentController;
 
@@ -14,4 +25,32 @@ public class RightController {
 
     @FXML
     private RequestDetailsController requestDetailsController;
+
+    @FXML
+    private Button computeRoutes;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        computeRoutes.setOnAction(e -> onComputeRoutesPressed());
+    }
+
+    private void onComputeRoutesPressed(){
+        PlanningRequest planningRequest = parentController.getLeftController().getMapController().getPlanningRequest();
+
+        if(Objects.isNull(planningRequest)){
+            parentController.displayNotification("Pas de planning request chargé");
+            return;
+        }
+
+        try {
+            for (DeliveryGuy deliveryGuy : planningRequest.getCouriersMap().values()) {
+                deliveryGuy.getRoute().computeBestRequestsOrder();
+                deliveryGuy.getRoute().computeBestRoute();
+            }
+        }catch (IHMException e){
+            parentController.displayNotification(e.getMessage());
+        }
+
+        parentController.displayNotification(planningRequest.getCouriersMap().values().stream().findFirst().get().getRoute().getRequestsOrdered().size() + "");
+    }
 }
