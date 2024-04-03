cb678e12c6854b6e25558377181d432b1115a78e
Eren Mustafa C <pro.erenmustafa.ceylan@gmail.com>
2024-04-03 10:13:36 +0200

    Ajout du header et footer + Logo en compresser

--- src/main/java/fr/insa/geofast/services/PdfGenerator.java
+++ src/main/java/fr/insa/geofast/services/PdfGenerator.java
@@ -2,15 +2,25 @@ package fr.insa.geofast.services;
 
 import com.itextpdf.io.exceptions.IOException;
 
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.List;
 import com.itextpdf.layout.element.ListItem;
 import lombok.extern.slf4j.Slf4j;
 
+import com.itextpdf.kernel.events.Event;
+import com.itextpdf.kernel.events.IEventHandler;
+import com.itextpdf.kernel.events.PdfDocumentEvent;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.properties.TextAlignment;
+
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
 import java.util.Date;
 
 
@@ -36,9 +46,16 @@ public class PdfGenerator {
         return "planning-request-" + new Date().getTime() + ".pdf";
     }
 
+    protected static String getFormattedDate() {
+        return new Date().toString();
+    }
+
     public void manipulatePdf(String dest) throws IOException, FileNotFoundException {
         PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
         Document document = new Document(pdf);
+        document.setTopMargin(50);
+        pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new TextFooterEventHandler(document));
+
 
         List topLevel = new List();
         ListItem topLevelItem = new ListItem();
@@ -60,6 +77,45 @@ public class PdfGenerator {
         document.add(topLevel);
 
         document.close();
+    }
+
+
+    private static class TextFooterEventHandler implements IEventHandler {
+        protected Document doc;
+
+        public TextFooterEventHandler(Document doc) {
+            this.doc = doc;
+        }
+
+        @Override
+        public void handleEvent(Event currentEvent) {
+            PdfDocumentEvent docEvent = (PdfDocumentEvent) currentEvent;
+            Rectangle pageSize = docEvent.getPage().getPageSize();
+
+            float leftX = pageSize.getLeft() + doc.getLeftMargin();
+            float rightX = ((pageSize.getLeft() + doc.getLeftMargin())
+                    + (pageSize.getRight() - doc.getRightMargin())) - 100;
+            float centerX = (pageSize.getLeft() + doc.getLeftMargin() + pageSize.getRight() - doc.getRightMargin()) / 2;
+            float headerY = pageSize.getTop() - doc.getTopMargin() + doc.getTopMargin() / 2;
+            float footerY = doc.getBottomMargin();
+
+            Image geoFastLogo = null;
+            try {
+                geoFastLogo = new Image(ImageDataFactory.create("src/main/resources/fr/insa/geofast/GeoFast-compressed.png"));
+                geoFastLogo.setFixedPosition(leftX + 20, headerY - 10);
+                geoFastLogo.setHeight(25);
+            } catch (MalformedURLException e) {
+                log.error(e.getMessage());
+            }
+
+            Canvas canvas = new Canvas(docEvent.getPage(), pageSize);
+            canvas
+                    .setFontSize(8)
+                    .add(geoFastLogo)
+                    .showTextAligned("Exporté le " + getFormattedDate(), rightX, headerY, TextAlignment.CENTER)
+                    .showTextAligned("GeoFast", centerX, footerY, TextAlignment.CENTER)
+                    .close();
+        }
     }
 
 
