a97d5b3455074fda39c4adfbb08e619e7e3585c6
Eren Mustafa C <pro.erenmustafa.ceylan@gmail.com>
2024-04-03 09:11:11 +0200

    Cacher les boutons export et importer programme lorsque c'est inutile

--- src/main/java/fr/insa/geofast/controller/HeaderController.java
+++ src/main/java/fr/insa/geofast/controller/HeaderController.java
@@ -38,6 +38,8 @@ public class HeaderController implements Initializable {
         importerPlan.setOnAction(e -> readMapXml());
 
         importerProgramme.setOnAction(e -> readPlanningRequestXml());
+        importerProgramme.setVisible(false);
+        exporterProgramme.setVisible(false);
     }
 
     private void readMapXml() {
@@ -54,6 +56,7 @@ public class HeaderController implements Initializable {
             Map map = MapFactory.buildMap(selectedFile.getAbsolutePath());
             parentController.getMapController().displayMap(map);
             parentController.getMapController().setMap(map);
+            importerProgramme.setVisible(true);
         } catch (IHMException e) {
             parentController.getParentController().displayNotification(e.getMessage());
         }
@@ -76,6 +79,7 @@ public class HeaderController implements Initializable {
             parentController.getParentController().getRightController().getPlanningRequestsController().displayPlanningRequest(planningRequest);
             parentController.getMapController().displayPlanningRequest(planningRequest);
             parentController.getMapController().setPlanningRequest(planningRequest);
+            exporterProgramme.setVisible(true);
         } catch (IHMException e) {
             parentController.getParentController().displayNotification(e.getMessage());
         }
