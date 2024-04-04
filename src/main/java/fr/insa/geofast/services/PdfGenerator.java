4f1c14886df3e063bb4062036625982b95f54f9e
mathicode <mathis.devidal@gmail.com>
2024-04-04 22:08:28 +0200

    basic overall structure implementation

--- src/main/java/fr/insa/geofast/services/PdfGenerator.java
+++ src/main/java/fr/insa/geofast/services/PdfGenerator.java
@@ -3,15 +3,21 @@ package fr.insa.geofast.services;
 import com.itextpdf.io.exceptions.IOException;
 
 import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
+import com.itextpdf.kernel.pdf.colorspace.PdfColorSpace;
+import com.itextpdf.kernel.pdf.tagging.StandardRoles;
 import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Image;
-import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.element.List;
-import com.itextpdf.layout.element.ListItem;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.borders.SolidBorder;
+import com.itextpdf.layout.element.*;
+import com.itextpdf.layout.properties.*;
 import lombok.extern.slf4j.Slf4j;
 
 import com.itextpdf.kernel.events.Event;
@@ -19,7 +25,6 @@ import com.itextpdf.kernel.events.IEventHandler;
 import com.itextpdf.kernel.events.PdfDocumentEvent;
 import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.layout.Canvas;
-import com.itextpdf.layout.properties.TextAlignment;
 
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
@@ -71,27 +76,99 @@ public class PdfGenerator {
             log.error(e.getMessage());
         }
 
+        Paragraph title = new Paragraph("Programme de livraison")
+                .setFontColor(new DeviceRgb(0,0,0))
+                .setFontSize(26f)
+                .setBold();
+        document.add(
+                new Table(new float[]{pageSize.getWidth() - document.getLeftMargin() - document.getRightMargin()})
+                        .addCell(new Cell().add(title).setBorder(Border.NO_BORDER))
+                        .setTextAlignment(TextAlignment.CENTER)
+        );
 
-        List topLevel = new List();
-        ListItem topLevelItem = new ListItem();
-        topLevelItem.add(new Paragraph().add("Item 1"));
-        topLevel.add(topLevelItem);
-
-        List secondLevel = new List();
-        secondLevel.add("Sub Item 1");
-        ListItem secondLevelItem = new ListItem();
-        secondLevelItem.add(new Paragraph("Sub Item 2"));
-        secondLevel.add(secondLevelItem);
-        topLevelItem.add(secondLevel);
-
-        List thirdLevel = new List();
-        thirdLevel.add("Sub Sub Item 1");
-        thirdLevel.add("Sub Sub Item 2");
-        secondLevelItem.add(thirdLevel);
-
-        document.add(topLevel);
+        for (int i = 0; i < 5; i++)
+        {
+            addDeliveryGuyProgram(document, pageSize, i);
+        }
 
         document.close();
+    }
+
+    private void addDeliveryGuyProgram(Document document, PageSize pageSize, int index)
+    {
+
+        float[] columnsWidth = {150f, pageSize.getWidth() - 150f};
+
+        Table deliveryGuyHeader = new Table(columnsWidth);
+        deliveryGuyHeader.addCell(new Cell().add(new Paragraph("Livreur "+index).setBold().setFontSize(18f)).setVerticalAlignment(VerticalAlignment.BOTTOM).setBorder(Border.NO_BORDER));
+        deliveryGuyHeader.addCell(new Cell().add(new Paragraph("Départ du dépôt : 8h").setFontSize(14f)).setVerticalAlignment(VerticalAlignment.BOTTOM).setBorder(Border.NO_BORDER));
+
+        document.add(deliveryGuyHeader);
+        SolidLine solidLine = new SolidLine(1f);
+        document.add(new LineSeparator(solidLine));
+
+        for(int i = 0; i < 5; i++)
+        {
+            addDeliveryRequest(document, pageSize, i);
+        }
+    }
+
+    private void addDeliveryRequest(Document document, PageSize pageSize, int index)
+    {
+        Table requestHeader = new Table(new float[]{50f, (pageSize.getWidth()-50f)/2, (pageSize.getWidth()-50f)/2});
+        requestHeader.addCell(
+            new Cell().add(
+                new Paragraph(""+index)
+                    .setHorizontalAlignment(HorizontalAlignment.CENTER)
+                    .setVerticalAlignment(VerticalAlignment.MIDDLE)
+                    .setHeight(30f)
+                    .setWidth(30f)
+                    .setFontSize(20f)
+                    .setBold()
+                    .setBorder(new SolidBorder(2))
+                .setBorderRadius(new BorderRadius(100))
+            )
+            .setTextAlignment(TextAlignment.CENTER)
+            .setBorder(Border.NO_BORDER)
+        );
+        Table requestTimeInfo = new Table(new float[]{ (pageSize.getWidth()-50f)/2});
+
+        requestTimeInfo.addCell(new Cell().add(new Paragraph("Arrivée prévue à 8:20")).setBorder(Border.NO_BORDER).setPadding(0));
+        requestTimeInfo.addCell(new Cell().add(new Paragraph("Temps de livraison : 2min")).setBorder(Border.NO_BORDER).setPadding(0));
+
+        requestHeader.addCell(new Cell().add(requestTimeInfo).setVerticalAlignment(VerticalAlignment.MIDDLE).setBorder(Border.NO_BORDER));
+        requestHeader.addCell(new Cell().add(new Paragraph("lat. : 45.002011; lon. : 2.645644")).setVerticalAlignment(VerticalAlignment.MIDDLE).setBorder(Border.NO_BORDER));
+
+        document.add(requestHeader);
+
+        addRoute(document, pageSize);
+        SolidLine solidLine = new SolidLine(1f);
+        document.add(new LineSeparator(solidLine));
+    }
+
+    private void addRoute(Document document, PageSize pageSize){
+
+        Table route = new Table(new float[]{pageSize.getWidth() - document.getLeftMargin() - document.getRightMargin()})
+                .setHorizontalBorderSpacing(0)
+                .setVerticalBorderSpacing(0);
+
+        Paragraph routeLabel = new Paragraph("Itinéraire").setBold().setFontSize(14f).setMarginLeft(20f);
+        route.addCell(new Cell().add(routeLabel).setBorder(Border.NO_BORDER).setPaddings(0,0,0,0));
+        for(int i = 0; i < 5 ;i++){
+
+            Paragraph step = new Paragraph("Emprunter rue "+i).setFontSize(12f).setMarginLeft(40f);
+            Paragraph next = new Paragraph("puis").setFontSize(10f).setMarginLeft(40f).setFontColor(ColorConstants.GRAY);
+
+            route.addCell(new Cell().add(step).setBorder(Border.NO_BORDER).setPadding(0));
+            if (i < 5-1) {
+                route.addCell(new Cell().add(next).setBorder(Border.NO_BORDER).setPadding(0));
+            }
+
+        }
+        Paragraph arrivalLabel = new Paragraph("Arrivée").setBold().setFontSize(14f).setMarginLeft(20f);
+        route.addCell(new Cell().add(arrivalLabel).setBorder(Border.NO_BORDER).setPadding(0));
+
+        document.add(route);
     }
 
 
