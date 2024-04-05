2963002a52f8b52b6e6fc348e5535254e24ef3e7
Nassim MANSOUR <mansournassim7@gmail.com>
2024-04-05 05:19:13 +0200

    Ajout d'une notification montrant à l'utilisateur que les tournées ont été calculées avec succès.

--- src/main/java/fr/insa/geofast/controller/RightController.java
+++ src/main/java/fr/insa/geofast/controller/RightController.java
@@ -56,6 +56,7 @@ public class RightController implements Initializable {
         }
 
         // Le calcul a réussi
+        parentController.displayNotification("Tournées calculées avec succès", Styles.SUCCESS);
         mapController.updateLabels(planningRequest);
         mapController.displayComputedRoutes(planningRequest);
         parentController.getLeftController().getHeaderController().setExportButtonVisible(true);
